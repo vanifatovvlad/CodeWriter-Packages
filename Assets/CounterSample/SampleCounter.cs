651fc4f4ef9f4188647478a481418bcbade195ad
VladV <vanifatov.vlad@yandex.ru>
2020-08-03 10:17:15 +0300

    Add Counter sample

---
+++ Assets/CounterSample/SampleCounter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UniMob;
+using UnityEngine.UI;
+
+namespace Sample
+{
+    using UnityEngine;
+
+    public class SampleCounter : MonoBehaviour
+    {
+        [SerializeField] private Text counterText = default;
+        [SerializeField] private Button incrementButton = default;
+
+        [Atom] private int Counter { get; set; } = 0;
+
+        private IDisposable _render;
+
+        private void OnEnable()
+        {
+            _render = Atom.AutoRun(Render);
+
+            incrementButton.onClick.AddListener(Increment);
+        }
+
+        private void OnDisable()
+        {
+            _render.Dispose();
+
+            incrementButton.onClick.RemoveListener(Increment);
+        }
+
+        private void Render()
+        {
+            counterText.text = $"Count: {Counter}";
+        }
+
+        private void Increment()
+        {
+            Counter++;
+        }
+    }
+}
