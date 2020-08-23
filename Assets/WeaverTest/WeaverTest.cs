df0574efef02511acf158e3c3a6591692eadf2be
VladV <vanifatov.vlad@yandex.ru>
2020-08-23 18:02:37 +0300

    New weaver

---
+++ Assets/WeaverTest/WeaverTest.cs
@@ -0,0 +1,51 @@
+using System;
+using UniMob;
+using UnityEngine;
+
+namespace Sample
+{
+    public class WeaverTest
+    {
+        public int Source
+        {
+            get { return 1; }
+            set { Debug.Log(value); }
+        }
+        
+        [Atom]
+        public int Weaved
+        {
+            get { return 1; }
+            set { Debug.Log(value); }
+        }
+
+        private ComputedAtom<int> __Manual;
+
+        public int Manual
+        {
+            get
+            {
+                if (__Manual == null)
+                {
+                    __Manual = new ComputedAtom<int>("Sample.WeaverTest::Manual", () => Manual);
+                }
+
+                if (!__Manual.DirectEvaluate())
+                {
+                    return __Manual.Value;
+                }
+
+                return 1;
+            }
+            set
+            {
+                if (__Manual != null)
+                {
+                    __Manual.Invalidate();
+                }
+
+                Debug.Log(value);
+            }
+        }
+    }
+}
