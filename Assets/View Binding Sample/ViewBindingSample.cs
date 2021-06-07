cc4d44baa4d567a1f20269d6d485d774d973090b
VladV <vanifatov.vlad@yandex.ru>
2021-06-07 12:05:49 +0300

    Work

--- Assets/View Binding Sample/ViewBindingSample.cs
+++ Assets/View Binding Sample/ViewBindingSample.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using CodeWriter.ViewBinding;
+using UniMob;
 using UnityEngine;
 
 namespace View_Binding_Sample
@@ -18,23 +20,35 @@ namespace View_Binding_Sample
         [SerializeField]
         private ViewVariableString str = default;
 
+        [Atom] private bool SoundEnabled { get; set; }
+        [Atom] private bool MusicEnabled { get; set; }
+        [Atom] private string Str { get; set; }
+        [Atom] private float Volume { get; set; }
+
         private IEnumerator Start()
         {
+            soundEnabled.SetSource(Atom.Computed(() => SoundEnabled));
+            musicEnabled.SetSource(Atom.Computed(() => MusicEnabled));
+            str.SetSource(Atom.Computed(() => Str));
+            volume.SetSource(Atom.Computed(() => Volume));
+
             while (enabled)
             {
-                yield return new WaitForSeconds(0.2f);
-                soundEnabled.Value = true;
-                musicEnabled.Value = false;
+                yield return new WaitForSeconds(0.02f);
+                SoundEnabled = true;
+                MusicEnabled = false;
 
-                yield return new WaitForSeconds(0.2f);
-                soundEnabled.Value = false;
-                musicEnabled.Value = true;
+                yield return new WaitForSeconds(0.02f);
+                SoundEnabled = false;
+                MusicEnabled = true;
 
-                yield return new WaitForSeconds(0.2f);
-                str.Value = "Hello!";
+                Volume = (Time.time % 1) * 100;
 
-                yield return new WaitForSeconds(0.2f);
-                str.Value = "WORLD";
+                yield return new WaitForSeconds(0.02f);
+                Str = "Hello!";
+
+                yield return new WaitForSeconds(0.02f);
+                Str = "WORLD";
             }
         }
     }
